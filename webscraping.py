df4bfec6c2c6bb736ed6bee7bdf82b8337bbbc3a
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-10-20 19:54:28 -0400

    extract articles from news pages

--- webscraping.py
+++ webscraping.py
@@ -2,18 +2,29 @@ from urllib import request
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://globalnews.ca/tag/canadian-parliament/"
-
+url = "https://www.cbc.ca/news/world"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
-# print(doc.prettify())
+
+
+def extract_article(url):
+    subResult = requests.get(url)
+    subDoc = BeautifulSoup(subResult.text, "html.parser")
+    article = ""
+
+    for a in subDoc.find_all('p', {"class": None}):
+        if (a.text.find("Audience Relations, CBC") != -1):
+            break
+        else:
+            article = article + a.text
+
+    return article
+
+
 urlList = []
 for a in doc.find_all('a', href=True, ):
-    if (a['href'].find('/news') != -1):
+    if (a['href'].find('/news/world') != -1):
         print("Found the URL:", a['href'])
         urlList.append(a['href'])
 
-
-subResult = requests.get(urlList[0])
-subDoc = BeautifulSoup(subResult.text, "html.parser")
-print(subDoc.prettify())
+print(extract_article("https://www.cbc.ca" + urlList[0]))
