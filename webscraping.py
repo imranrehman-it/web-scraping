07efcae478c7358dd7ac3461b714670303acdb71
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-10-20 20:41:01 -0400

    file writing

--- webscraping.py
+++ webscraping.py
@@ -2,6 +2,7 @@ from urllib import request
 from bs4 import BeautifulSoup
 import requests
 
+
 url = "https://www.cbc.ca/news/world"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
@@ -13,6 +14,7 @@ def extract_article(url):
     article = ""
 
     for a in subDoc.find_all('p', {"class": None}):
+        # gets all <p> up until end of article denoted by Audience Relations
         if (a.text.find("Audience Relations, CBC") != -1):
             break
         else:
@@ -21,10 +23,18 @@ def extract_article(url):
     return article
 
 
+# extracts all links to news article from home page to be later accessed for article extraction
 urlList = []
 for a in doc.find_all('a', href=True, ):
     if (a['href'].find('/news/world') != -1):
         print("Found the URL:", a['href'])
         urlList.append(a['href'])
 
-print(extract_article("https://www.cbc.ca" + urlList[0]))
+
+def write_article(article):
+    with open("articles.txt", 'w') as f:
+        f.write(extract_article("https://www.cbc.ca" + urlList[4]) + "\n")
+    f.close()
+
+
+print(extract_article("https://www.cbc.ca" + urlList[4]))
